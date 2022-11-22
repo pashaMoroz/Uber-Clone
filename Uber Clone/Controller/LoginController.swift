057fb9eea01894245aa234977bba884e25464e1e
pashaMoroz <vferme2015@gmail.com>
2022-11-22 14:47:42 -0500

    setup project

--- Uber Clone/Controller/LoginController.swift
+++ Uber Clone/Controller/LoginController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  Uber Clone
 //
 //  Created by Admin on 22/11/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
 }
-
